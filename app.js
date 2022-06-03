5d31c8655fb13c00a0b65ac13ac0a6453765bbd8
Allison Ause <allison.ause@gmail.com>
2022-06-03 10:23:21 -0700

    Commit of code for working, tested Name Input and also working tested Image Select

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // grab DOM elements
+const nameInput = document.getElementById('name-input');
+const nameHeader = document.getElementById('name-header');
+
+const iconSelect = document.getElementById('icon-select');
+const iconDisplay = document.getElementById('icon-display');
+
 
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
+
+nameInput.addEventListener('input', () => {
+    nameHeader.textContent = nameInput.value;
+});
+
+iconSelect.addEventListener('change', () => {
+    const src = 'assets/' + iconSelect.value;
+    iconDisplay.src = src;
+});
